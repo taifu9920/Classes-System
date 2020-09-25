bb547dff8ee702ad23c04608ac57863131db87b5
黎サブ 星野 <taifu9920923@gmail.com>
2020-09-25 09:33:24 +0800

    Updates

--- Main.py
+++ Main.py
@@ -1,51 +1,60 @@
 from flask import Flask, request, render_template, session, redirect, url_for
 from markupsafe import escape
 from flask_wtf.csrf import CSRFProtect
-from datetime import datetime
-import pytz
+from datetime import datetime, time
 from tinydb import TinyDB, Query
-import requests, bs4, os
+import requests, bs4, os, pytz
 
 from config import *
 
-db = TinyDB("db.tinydb")
+class TDB():
+    def __init__(self):
+        self.db = TinyDB("db.tinydb")
+    def Save(self):
+        self.db.close()
+        self.db = TinyDB("db.tinydb")
+
+tinydb = TDB()
 query = Query()
 tw = pytz.timezone('Asia/Taipei')
 d_start = datetime(2020,9,14,0,0,0,0,tw)
-max_week = 18
 
-Codes = {
-    "B01":"綜合大樓",
-    "C01":"工學院",
-    "C02":"理學院",
-    "K01":"體育休閒大樓",
-    "L01":"圖資大樓",
-    "L02":"法學院",
-    "M01":"管學院",
-    "H1-":"人社科學院",
-    "H2-":"人社科學院"
-    }
 
-Log = str(datetime.now())[:-7].replace(" ", "_").replace(":","_") + ".log"
+#Logger file path
+Log = str(datetime.now(tw))[:-7].replace(" ", "_").replace(":","_") + ".log"
 # ----- Functions -----
 def gen_checkbox(id, week, holder, value):
     return '<dd><label for="{0}">第{1}周</label><input type="checkbox" id="{0}" class="w3-check checkbox" {4}/><input name="{0}" class="w3-input w3-border" type="text" placeholder="{2}" value="{5}" {3}></dd>'.format(id, week, holder, "" if value else "disabled", "checked" if value else "", value)
 
-def getWeek():
-    return str(((datetime.now(tw) - d_start).days // 7) + 1)
+#Compare if the time is between two time object
+TimeCompare = lambda obj, val: obj[0] < val and val < obj[1]
 
+#Get the week since started classes
+getWeek = lambda: str(((datetime.now(tw) - d_start).days // 7) + 1)
+
+#Get all class datas
+def Classdata(sess):
+    resp = sess.get("https://course.nuk.edu.tw/Sel/query3.asp")
+    resp.encoding = "big5"
+    info = bs4.BeautifulSoup(resp.text, "lxml")
+    classes = [[o.string for o in i.find_all("td")] for i in info.table.find_all("tr")[1:]]
+    return {i[1]:i for i in classes if i[9] == "選上"}
+
+#Log Messages
 def logger(msg, code = 0):
     #0 to 4 are available
     FolderInit(LoggerPath)
     with open(LoggerPath + Log, "a+", encoding='UTF-8') as file:
-        buffer = "{0} | {1} : {2}".format(types[code], datetime.now(), msg)
+        buffer = "{0} | {1} : {2}".format(types[code], datetime.now(tw), msg)
         print(buffer)
         file.write(buffer + "\n")
 
+#Init a Path Folder
 def FolderInit(path):
     if not PathExist(path):
         os.makedirs(path)
 
+#Login to class selection system and get Session
 def Auth(acc, psw):
     sess = requests.session()
     resp = sess.post("https://course.nuk.edu.tw/Sel/SelectMain1.asp", {"Account": acc, "Password": psw})
@@ -54,19 +63,36 @@ def Auth(acc, psw):
     if info.b.text == "帳號、密碼有誤，請確認後再重新登入！":
         logger("A failed login.")
         return None
+    session["acc"] = acc
+    session["psw"] = psw
+    session["login"] = "Yes"
+    #Also init any classes that don't have datas
+    datas = Classdata(sess)
+    for i in datas.items():
+        if not tinydb.db.get(query[session["acc"]][i[0]].exists()):
+            tinydb.db.insert({session["acc"] : {i[0]: {"weeks": max_week}}})
+            room = i[1][6].split(",")[0].strip()
+            if room == "": room = "無標記教室"
+            for o in range(1, max_week + 1):
+                for j in Codes.items():
+                    room = room.replace(j[0], j[1])
+                tinydb.db.insert({session["acc"]:{i[0]: {"room" + str(o): room}}})
     return sess
 
 PathExist = lambda path: os.path.exists(path)
+#Connection message
 incoming = lambda req: logger("Incoming connection from {0}, target page {1}".format(req.remote_addr, req.path), 1)
 # ----- End of Functions -----
 
 # ----- Flask Programs -----
 app = Flask(__name__, static_folder='templates/static', 
 template_folder='templates', )
-secret = db.get(query.secret.exists())
-if secret == None: secret = os.urandom(24).hex() ; db.insert({"secret": secret})
+#Secret generating and saving
+secret = tinydb.db.get(query.secret.exists())
+if secret == None: secret = os.urandom(24).hex() ; tinydb.db.insert({"secret": secret})
 else: secret = secret["secret"]
 app.config['SECRET_KEY'] = secret
+#CSRF Protect for flask
 CSRFProtect(app)
 
 @app.route("/")
@@ -87,10 +113,7 @@ def login():
         data = request.form
         if data.get("acc") and data.get("psw"):
             #Auth
-            if Auth(data["acc"], data["psw"]): 
-                session["acc"] = data["acc"]
-                session["psw"] = data["psw"]
-                session["login"] = "Yes"
+            if Auth(data["acc"], data["psw"]):
                 return redirect(url_for("Root"))
             return "Invaild Login Informations"
     return render_template("login.html", week=getWeek())
@@ -103,6 +126,7 @@ def logout():
     session.pop("login", None)
     return redirect(url_for("Root"))
 
+#Planning to remove in final state
 @app.route("/WIP")
 def classes():
     incoming(request)
@@ -113,49 +137,76 @@ def classView():
     incoming(request)
     if session.get("login"):
         sess = Auth(session["acc"], session["psw"])
-        resp = sess.get("https://course.nuk.edu.tw/Sel/roomlist1.asp")
-        resp.encoding = "big5"
-        info = bs4.BeautifulSoup(resp.text, "lxml")
-        classes = info.table
-        classes["class"] = "w3-table w3-striped w3-border w3-gray"
-        classes.tr["class"] = "w3-green"
-        classes["style"] = None
-        for i in classes.find_all("tr")[1:]:
-            for o in i.find_all("td")[:2]:
-                o["class"] = "w3-green"
-            for o in i.find_all("td")[2:]:
-                #All classes
-                if o.string == None:
+        if sess:
+            resp = sess.get("https://course.nuk.edu.tw/Sel/roomlist1.asp")
+            resp.encoding = "big5"
+            info = bs4.BeautifulSoup(resp.text, "lxml")
+            classes = info.table
+            classes["class"] = "w3-table w3-striped w3-border w3-gray"
+            classes.tr["class"] = "w3-green"
+            classes["style"] = None
+            data = Classdata(sess)
+            for i in classes.find_all("tr")[1:]:
+                for o in i.find_all("td")[:2]:
+                    o["class"] = "w3-green"
+                for o in [o for o in i.find_all("td")[2:] if o.contents[0].string and o.contents[0].string != "\u3000"]:
+                    #All classes
                     classid = o.contents[0].string
-                    weeks = db.get(query[session["acc"]][classid].weeks.exists())
-                    if weeks: weeks = weeks[session["acc"]][classid]["weeks"]
-                    else: db.insert({session["acc"] : {classid: {"weeks": max_week}}}) ; weeks = max_week
+                    weeks = tinydb.db.get(query[session["acc"]][classid].weeks.exists())[session["acc"]][classid]["weeks"]
                     if int(getWeek()) <= weeks:
                         if str(o.contents[-1]) != "<br/>":
                             #Classes that has room
+                            #Translate classroom
                             for j in Codes.items():
                                 o.contents[-1].replace_with(o.contents[-1].replace(j[0], j[1]))
-                            temp = o.contents[-1].split(",")
-                            room = db.get(query[session["acc"]][classid]["room" + getWeek()].exists())
-                            if room: room = room[session["acc"]][classid]["room" + getWeek()]
-                            else: room = ""
-                            o.contents[-1].replace_with(room)
-
-                            if len(temp) > 1:
-                                temp[0] = "(" + temp[0]
-                                temp[-1] = temp[-1] + ")"
-                                for j in temp:
-                                    if room: o.contents.append(info.new_tag("br"))
-                                    else: room = True
-                                    o.contents.append(info.new_string(j))
-                            else:
-                                if room != temp[0]:
+                            room = tinydb.db.get(query[session["acc"]][classid]["room" + getWeek()].exists())
+                            if room:
+                                room = room[session["acc"]][classid]["room" + getWeek()]
+                                temp = [i for i in o.contents[-1].split(",") if i != room]
+                                o.contents[-1].replace_with(room)
+                                if len(temp) > 1:
+                                    temp[0] = "(" + temp[0]
+                                    temp[-1] = temp[-1] + ")"
+                                    for j in temp:
+                                        if room: o.contents.append(info.new_tag("br"))
+                                        else: room = True
+                                        o.contents.append(info.new_string(j))
+                                elif len(temp) == 1:
                                     if room: o.contents.append(info.new_tag("br"))
                                     o.contents.append(info.new_string("(%s)" % temp[0]))
-                    else:
-                        o.contents = []
-                    
-        return render_template("class.html", Version = "1.0.0", Tables = classes, ContentAttri = "id='ClassesContent'", week=getWeek())
+                                data[classid][6] = "".join([str(i) for i in o.contents[2:]]).strip()
+                            else: 
+                                o.contents = []
+                                if data.get(classid): data[classid][6] = ""
+                        else:
+                            room = tinydb.db.get(query[session["acc"]][classid]["room" + getWeek()].exists())
+                            if room:
+                                room = room[session["acc"]][classid]["room" + getWeek()]
+                                o.contents.append(info.new_string(room))
+                                data[classid][6] = room
+                            else: 
+                                o.contents = []
+                                if data.get(classid): data[classid][6] = ""
+                    else: o.contents = []
+            schedule = {i[1]:i[5] for i in data.values()}
+            week = datetime.now(tw).weekday()
+            now_time = datetime.now(tw).time()
+            nextID = stat = start = None # - stat - : 0 = before, 1 = in class
+            begin = week 
+            while not nextID:
+                for i in [i for i in schedule.items() if Date[week] in i[1]]:
+                    for o in [o for o in Timer.items() if str(o[0]) in i[1]]:
+                        if TimeCompare(o[1], now_time) or now_time < o[1][0]:
+                            nextID = i[0]
+                            stat = 0 if now_time < o[1][0] else 1
+                            start = o[1][0] if now_time < o[1][0] else o[1][1]
+                            break
+                week = ( week + 1 ) % 7
+                if week == begin: break
+            note = tinydb.db.get(query[session["acc"]][nextID]["note" + getWeek()].exists())
+            if note: note = note[session["acc"]][nextID]["note" + getWeek()]
+            nextclass = ((("{0}下課<br>正在上{1}" if stat else "{0}上課<br>下堂課是{1}") + "，位於{2}").format(start.strftime("%H點%M分") ,data[nextID][2], data[nextID][6]) + (("<br>備註：%s" % note) if note else "")) if nextID else "無匹配課程...?!"
+            return render_template("class.html", Version = "1.0.0", Tables = classes, ContentAttri = "id='ClassesContent'", week=getWeek(), NextClass=nextclass)
     logger("Not login! Redirect to Login page")
     return redirect(url_for("login"))
 
@@ -165,29 +216,29 @@ def Manager():
     if session.get("login"):
         if request.method == "GET":
             sess = Auth(session["acc"], session["psw"])
-            resp = sess.get("https://course.nuk.edu.tw/Sel/query3.asp")
-            resp.encoding = "big5"
-            info = bs4.BeautifulSoup(resp.text, "lxml")
-            classes = info.table
-            del classes["style"]
-            del classes["border"]
-            classes["class"] = "w3-table w3-striped w3-gray"
-            classes.tr["class"] = "w3-red"
-            for i in classes.find_all("tr")[1:]:
-                if i.find_all("td")[-1].string != "選上": del i
-                else:
-                    button = info.new_tag("input")
-                    button["type"] = "submit" ; button["class"] = "w3-button w3-green" ; button["name"] = "ClassID" ; button["value"] = i.find_all("td")[1].string
-                    i.find_all("td")[1].string.replace_with(button)
-            return render_template("classlist.html", Version = "1.0.0", Classlist = classes, week=getWeek())
+            if sess:
+                resp = sess.get("https://course.nuk.edu.tw/Sel/query3.asp")
+                resp.encoding = "big5"
+                info = bs4.BeautifulSoup(resp.text, "lxml")
+                classes = info.table
+                del classes["style"], classes["border"]
+                classes["class"] = "w3-table w3-striped w3-gray"
+                classes.tr["class"] = "w3-red"
+                for i in classes.find_all("tr")[1:]:
+                    if i.find_all("td")[-1].string == "選上":
+                        button = info.new_tag("input")
+                        button["type"] = "submit" ; button["class"] = "w3-button w3-green"
+                        button["name"] = "ClassID" ; button["value"] = i.find_all("td")[1].string
+                        i.find_all("td")[1].string.replace_with(button)
+                    else: del i
+                return render_template("classlist.html", Version = "1.0.0", Classlist = classes, week=getWeek())
         else:
             data = request.form
             if data.get("ClassID") and session.get("login"):
                 return redirect(url_for("EditClass", classID = data["ClassID"]))
-    else:
-        logger("Not login! Redirect to Login page")
-        return redirect(url_for("login"))
-    return render_template("index.html", Version = "1.0.0", week=getWeek())
+            return "Where is your 'ClassID'?"
+    logger("Not login! Redirect to Login page")
+    return redirect(url_for("login"))
 
 @app.route("/manage/<string:classID>", methods=['GET', 'POST'])
 def EditClass(classID):
@@ -195,26 +246,17 @@ def EditClass(classID):
         classID = escape(classID)
         if request.method == "GET":
             sess = Auth(session["acc"], session["psw"])
-            if sess:
-                resp = sess.get("https://course.nuk.edu.tw/Sel/query3.asp")
-                resp.encoding = "big5"
-                info = bs4.BeautifulSoup(resp.text, "lxml")
-                classes = [[o.string for o in i.find_all("td")] for i in info.table.find_all("tr")[1:]]
-                datas = {i[1]:i for i in classes}
-                weeks = db.get(query[session["acc"]][classID].weeks.exists())
-                if weeks: weeks = weeks[session["acc"]][classID]["weeks"]
-                else: db.insert({session["acc"] : {classID: {"weeks": max_week}}}) ; weeks = max_week
-                classrooms = ""
-                classinfos = ""
+            weeks = tinydb.db.get(query[session["acc"]][classID].weeks.exists())
+            if sess and weeks:
+                datas = Classdata(sess)
+                weeks = weeks[session["acc"]][classID]["weeks"]
+                classrooms = classinfos = ""
                 for i in range(1,weeks+1):
-                    room = db.get(query[session["acc"]][classID]["room" + str(i)].exists())
-                    note = db.get(query[session["acc"]][classID]["note" + str(i)].exists())
+                    room = tinydb.db.get(query[session["acc"]][classID]["room" + str(i)].exists())
+                    note = tinydb.db.get(query[session["acc"]][classID]["note" + str(i)].exists())
                     checked = bool(room)
                     if room: room = room[session["acc"]][classID]["room" + str(i)]
-                    else: 
-                        room = datas[classID][6].split(",")[0]
-                        for o in Codes.items():
-                            room = room.replace(o[0], o[1])
+                    else: room = ""
                     if note: note = note[session["acc"]][classID]["note" + str(i)]
                     else: note = ""
                     classrooms += gen_checkbox("room"+str(i), i, "教室位置", room)
@@ -223,19 +265,20 @@ def EditClass(classID):
         else:
             data = request.form
             weeks = data.get("weeks")
-            if not weeks: weeks = max_week
+            if not weeks or not(weeks in range(0, max_week)): weeks = max_week
             else: weeks = int(weeks)
-            db.remove(query[session["acc"]][classID].exists())
-            db.insert({session["acc"] : {classID: {"weeks": weeks}}})
+            tinydb.db.remove(query[session["acc"]][classID].exists())
+            tinydb.db.insert({session["acc"] : {classID: {"weeks": weeks}}})
             for i in range(1, weeks+1):
                 room = data.get("room" + str(i))
                 note = data.get("note" + str(i))
-                if room: db.insert({session["acc"]:{classID: {"room" + str(i): room}}})
-                if note: db.insert({session["acc"]:{classID: {"note" + str(i): note}}})
-                
+                if room: tinydb.db.insert({session["acc"]:{classID: {"room" + str(i): room}}})
+                if note: tinydb.db.insert({session["acc"]:{classID: {"note" + str(i): note}}})
+            tinydb.Save()
             return redirect(url_for("EditClass", classID = classID))
     logger("Not login! Redirect to Login page")
     return redirect(url_for("login"))
+
 if ssl: app.run(host = "0.0.0.0", port = "9487", ssl_context = ssl)
 else: app.run(host = "0.0.0.0", port = "9487")
 
--- config.py
+++ config.py
@@ -1,4 +1,45 @@
+# --- Don't change if you don't know what you doing. ---
+from datetime import time
+#Log type
 types = ["Info", "Warning", "Critical", "Success", "Failed"]
+#Path to save logs
 LoggerPath = "Logs/"
 #ssl = ("../Certs/cert.pem", "../Certs/privkey.pem")
 ssl = None
+#Limit of weeks in a class
+max_week = 18
+
+#Building name of each classrooms
+Codes = {
+    "B01":"綜合大樓",
+    "C01":"工學院",
+    "C02":"理學院",
+    "K01":"體育休閒大樓",
+    "L01":"圖資大樓",
+    "L02":"法學院",
+    "M01":"管學院",
+    "H1-":"人社科學院",
+    "H2-":"人社科學院" 
+    }
+
+#Time of classes
+Timer = {
+    "X":[time(7, 5), time(7, 55)], #早7
+    1:[time(8, 5), time(8, 55)],
+    2:[time(9, 5), time(9, 55)],
+    3:[time(10, 10), time(11, 0)],
+    4:[time(11, 10), time(12, 0)],
+    "Y":[time(12, 10), time(13, 0)], #午休
+    5:[time(13, 10), time(14, 0)],
+    6:[time(14, 10), time(15, 0)],
+    7:[time(15, 15), time(16, 5)],
+    8:[time(16, 20), time(17, 10)],
+    9:[time(17, 20), time(18, 10)],
+    10:[time(18, 20), time(19, 10)],
+    11:[time(19, 15), time(20, 5)],
+    12:[time(20, 10), time(21, 0)],
+    13:[time(21, 5), time(21, 55)]
+    }
+
+#Date to integer, program needed, please don't touch dis-
+Date = list("一二三四五六日")
